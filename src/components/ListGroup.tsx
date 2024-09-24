de8d9ed8179b12f813d67ba94af0722c260c832c
DineshMitta <dineshgopimitta@gmail.com>
2024-09-24 22:20:40 +0530

    Changes in ListGroup,tsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = [
     "New York",
@@ -7,16 +9,24 @@ function ListGroup() {
     "London",
     "Paris",
   ];
-  items = [];
 
-  
+  const [selectedIndex, setselectedIndex] = useState(-1);
+
+ 
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
-      {items.length == 0 && <p>No Items Found</p> }
+      {items.length === 0 && <p>No Items Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={ selectedIndex === index ? 'list-group-item active' : 'list-group'}
+            key={item}
+            onClick={() => {setselectedIndex(index);}}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
